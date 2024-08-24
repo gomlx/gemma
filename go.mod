d9f05bd5650097fe3cf5022db7d73cff0c724544
Jan Pfeifer <pfeifer@gmail.com>
2024-08-24 08:37:35 +0200

    Added Config for transformers package.

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/gomlx/gemma
 go 1.23.0
 
 require (
+	github.com/eliben/go-sentencepiece v0.4.0
 	github.com/gomlx/gomlx v0.11.0
+	github.com/gomlx/gopjrt v0.2.3
+	github.com/janpfeifer/must v0.0.2
 	github.com/pkg/errors v0.9.1
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
 )
 
 require (
@@ -14,14 +18,12 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gomlx/exceptions v0.0.3 // indirect
-	github.com/gomlx/gopjrt v0.2.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/janpfeifer/gonb v0.10.2 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/schollz/progressbar/v3 v3.14.6 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/term v0.23.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
---
+++ transformers/config.go
@@ -0,0 +1,123 @@
+package transformers
+
+import (
+	"github.com/gomlx/gemma/trees"
+	"github.com/gomlx/gomlx/types/tensors"
+	"github.com/pkg/errors"
+	"maps"
+	"strings"
+)
+
+type GemmaType int
+
+const (
+	UnknownGemmaType GemmaType = iota
+	Gemma_2B
+	Gemma_7B
+	Gemma2_2B
+	Gemma2_9B
+	Gemma2_27B
+)
+
+//go:generate enumer -type=GemmaType -transform=snake -values -text -json -yaml config.go
+
+var numLayersToGemmaClass = map[int]GemmaType{
+	18: Gemma_2B,
+	28: Gemma_7B,
+	26: Gemma2_2B,
+	42: Gemma2_9B,
+	46: Gemma2_27B,
+}
+
+type AttentionType int
+
+//go:generate enumer -type=AttentionType -trimprefix=AttentionType -transform=snake -values -text -json -yaml config.go
+
+const (
+	AttentionTypeUnknown AttentionType = iota
+	AttentionTypeGlobal
+	AttentionTypeLocalSliding
+)
+
+// QueryPreAttentionNormalisationType defines how to normalize query before attention.
+type QueryPreAttentionNormalisationType int
+
+//go:generate enumer -type=QueryPreAttentionNormalisationType -trimprefix=QueryNormType -transform=snake -values -text -json -yaml config.go
+
+const (
+	// QueryNormTypeByOneOverSqrtHeadDim indicates whether to scale the query by 1/sqrt(head_dim)
+	QueryNormTypeByOneOverSqrtHeadDim QueryPreAttentionNormalisationType = iota
+
+	// QueryNormTypeByEmbedDimDivNumHeads indicates whether to scale the query by `embed_dim // num_heads`
+	QueryNormTypeByEmbedDimDivNumHeads
+
+	// QueryNormTypeByOneOverSqrtEmbedDimDivNumHeads indicates whether to scale the query by `1/sqrt(embed_dim // num_heads)`
+	QueryNormTypeByOneOverSqrtEmbedDimDivNumHeads
+)
+
+// Config Gemma transformer model.
+type Config struct {
+	Type                                 GemmaType
+	NumLayers                            int
+	NumEmbed, EmbedDim                   int
+	NumHeads, HeadDim                    int
+	HiddenDim                            int
+	NumKVHeads                           int
+	FinalLogitSoftCap                    float64
+	UsePostAttentionNorm, UsePostFFWNorm bool
+
+	AttentionTypes        []AttentionType
+	MaxCacheLength        int
+	QueryPreAttentionNorm QueryPreAttentionNormalisationType
+
+	AttentionLogitsSoftCap float64
+	SlidingWindowSize      int
+	TransposeGatingEinsum  bool
+}
+
+// NewConfigFromWeights creates a transformers config model, based on the structure of the loaded model weights.
+func NewConfigFromWeights(weights *trees.Tree[*tensors.Tensor]) (*Config, error) {
+	c := &Config{
+		MaxCacheLength:        1024,
+		QueryPreAttentionNorm: QueryNormTypeByOneOverSqrtHeadDim,
+	}
+
+	// Find number of layers:
+	for key := range maps.Keys(weights.Root.Map["transformer"].Map) {
+		if strings.Index(key, "layer") != -1 {
+			c.NumLayers++
+		}
+	}
+
+	if t, found := numLayersToGemmaClass[c.NumLayers]; found {
+		c.Type = t
+	}
+
+	switch c.Type {
+	case Gemma2_2B:
+		c.setGemma2_2B()
+	default:
+		return nil, errors.Errorf("unknown or not implemented for Gemma model type %q", c.Type)
+	}
+	return c, nil
+}
+
+func (c *Config) setGemma2_2B() {
+	c.NumLayers = 26
+	c.NumEmbed = 256128
+	c.EmbedDim = 2304
+	c.HiddenDim = 9216
+	c.NumHeads = 8
+	c.HeadDim = 256
+	c.NumKVHeads = 4
+	c.FinalLogitSoftCap = 30.0
+	c.AttentionTypes = nil
+	for _ = range c.NumLayers / 2 {
+		c.AttentionTypes = append(c.AttentionTypes, []AttentionType{AttentionTypeLocalSliding, AttentionTypeLocalSliding}...)
+	}
+	c.UsePostAttentionNorm = true
+	c.UsePostFFWNorm = true
+	c.QueryPreAttentionNorm = QueryNormTypeByOneOverSqrtHeadDim
+	c.AttentionLogitsSoftCap = 50.0
+	c.SlidingWindowSize = 4096
+}
