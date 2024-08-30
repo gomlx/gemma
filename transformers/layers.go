e7c5e3ef6d22e940abdcd9ee95acf912ff1f4b41
Jan Pfeifer <pfeifer@gmail.com>
2024-08-30 18:55:32 +0200

    Fixed RMSNorm Added KernelEinsum

--- transformers/layers.go
+++ transformers/layers.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gomlx/gomlx/types/shapes"
 )
 
+// KernelEinsum multiplies the input by a kernel of the given shape, using the given graph.EinSum equation.
+func KernelEinsum(ctx *context.Context, equation string, x *Node, kernelShape shapes.Shape) *Node {
+	g := x.Graph()
+	kernelVar := ctx.VariableWithShape("w", kernelShape)
+	kernel := kernelVar.ValueGraph(g)
+	kernel.SetLogged("EinSum::kernel")
+	return Einsum(equation, x, kernel)
+
+}
+
 // RMSNorm normalizes by its root-mean-square (x = x / (mean(sqrt(x), axis=-1) + epsilon)) and applies a learned scale.
 func RMSNorm(ctx *context.Context, x *Node) *Node {
 	g := x.Graph()
@@ -18,9 +28,9 @@ func RMSNorm(ctx *context.Context, x *Node) *Node {
 	scaleVar := ctx.WithInitializer(initializers.Zero).
 		VariableWithShape("scale", shapes.Make(x.DType(), x.Shape().Dim(-1)))
 	scale := scaleVar.ValueGraph(g)
-	// Scale centered on 1.0 (so 0.0 has no effect).
-	scale = OnePlus(scaleVar.ValueGraph(g))
 	scale = ExpandLeftToRank(scale, normalizedX.Rank()) // Expand rank of scale to match normalizedX.
-	normalizedX = Mul(x, scale)
+	// Scale centered on 1.0 (so 0.0 has no effect).
+	scale = OnePlus(scale)
+	normalizedX = Mul(scale, normalizedX)
 	return normalizedX
 }
