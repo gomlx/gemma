b4346bb0a59ce959071435b0bb83b000c0fb7a4f
Jan Pfeifer <pfeifer@gmail.com>
2024-08-31 09:28:44 +0200

    Added cache integration, attention calculation and sliding mask.

--- transformers/attention.go
+++ transformers/attention.go
@@ -1,6 +1,7 @@
 package transformers
 
 import (
+	"github.com/gomlx/exceptions"
 	"github.com/gomlx/gemma/trees"
 	. "github.com/gomlx/gomlx/graph"
 	"github.com/gomlx/gomlx/ml/context"
@@ -35,7 +36,25 @@ func createAttentionCache(data *trees.Tree[*tensors.Tensor], treePath trees.Path
 	return nil
 }
 
-func Attention(ctx *context.Context, config *Config, x, positions *Node, cache *trees.Tree[*Node], attentionMask *Node) *Node {
+// Must panics if the error is not nil.
+func Must(err error) {
+	panic(err)
+}
+
+// Must1 panics in case of error, otherwise returns the one return value.
+func Must1[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// Attention builds an attention layer, optionally using cache to store a limited amount of context.
+//
+// The attentionIdx indexes attention configuration (in config) parameters, like config.AttentionTypes.
+func Attention(ctx *context.Context, config *Config, attentionIdx int, x, positions *Node, cache *trees.Tree[*Node], attentionMask *Node) *Node {
+	g := x.Graph()
+
 	// Calculates projections used in the attention.
 	var queryProjection, keyProjection, valueProjection *Node
 	if config.UseQKV {
@@ -61,10 +80,69 @@ func Attention(ctx *context.Context, config *Config, x, positions *Node, cache *
 		valueProjection = Squeeze(Slice(kvProjections, AxisElem(1)), 0)
 	}
 
-	queryProjection.SetLogged("Attention::queryProjection")
-	keyProjection.SetLogged("Attention::keyProjection")
-	valueProjection.SetLogged("Attention::valueProjection")
+	queryProjection = ApplyRotaryPositionEncoding(queryProjection, positions, RoPEDefaultMaxWaveLength)
+	queryScaled := MulScalar(queryProjection, config.QueryPreAttentionScalar())
+	keyProjection = ApplyRotaryPositionEncoding(keyProjection, positions, RoPEDefaultMaxWaveLength)
 
+	queryScaled.SetLogged("Attention::queryScaledProjection(pre-cache)")
+	keyProjection.SetLogged("Attention::keyProjection(pre-cache)")
+	valueProjection.SetLogged("Attention::valueProjection(pre-cache)")
+
+	// If cache is set, update it with the projections of the slice of the sequence given, and then take the
+	// projections of the whole cache.
+	if cache != nil {
+		// Insert calculated projections in cache: cached projections are shaped [batchSize, maxCacheLength, numHeads, headDim]
+		endIndex, err := cache.Get("end_index")
+		if err != nil {
+			panic(err)
+		}
+		zeroIdx := ScalarZero(g, dtypes.Int32)
+		cacheSequencePosition := Mod(endIndex, Scalar(g, endIndex.DType(), config.MaxCacheLength))
+		updateSliceIndices := []*Node{zeroIdx, cacheSequencePosition, zeroIdx, zeroIdx}
+
+		valueProjection = DynamicUpdateSlice(Must1(cache.Get("v")), valueProjection, updateSliceIndices)
+		keyProjection = DynamicUpdateSlice(Must1(cache.Get("k")), keyProjection, updateSliceIndices)
+		Must(cache.Set(trees.Path{"v"}, valueProjection))
+		Must(cache.Set(trees.Path{"k"}, keyProjection))
+	}
+
+	batchSize := queryScaled.Shape().Dim(0) // B
+	seqLength := queryScaled.Shape().Dim(1) // T
+	numQueryHeads := queryScaled.Shape().Dim(2)
+	headDim := queryScaled.Shape().Dim(3) // H
+
+	var logits *Node
+	if config.UseGroupQueryAttention {
+		// There are fewer key (and value) projections than query projections,
+		// reshape matrices accordingly and adjust Einsum.
+		numKVHeads := config.NumKVHeads               // K
+		queryPerKVHeads := numQueryHeads / numKVHeads // G
+		// S = config.MaxCacheLength
+		queryScaled = Reshape(queryScaled, batchSize, seqLength, numKVHeads, queryPerKVHeads, headDim)
+		logits = Einsum("BTKGH,BSKH->BTKGS", queryScaled, keyProjection)
+		logits = Reshape(logits, batchSize, seqLength, numQueryHeads, config.MaxCacheLength)
+	} else {
+		// Same number of query/key projections.
+		// N = numQueryHeads == numKVHeads.
+		logits = Einsum("BTNH,BSNH->BTNS", queryScaled, keyProjection)
+	}
+
+	if config.AttentionLogitsSoftCap > 0 {
+		logits = Tanh(DivScalar(logits, config.AttentionLogitsSoftCap))
+		logits = MulScalar(logits, config.AttentionLogitsSoftCap)
+	}
+
+	if config.AttentionTypes[attentionIdx] == AttentionTypeLocalSliding {
+		if config.SlidingWindowSize <= 0 {
+			exceptions.Panicf("Config.SlidingWindowSize must be set for AttentionTypeLocalSliding")
+		}
+		allOnes := OnesLike(attentionMask)
+		slidingMask := And(
+			TakeUpperTriangular(allOnes, 1-config.SlidingWindowSize),
+			TakeLowerTriangular(allOnes, config.SlidingWindowSize-1),
+		)
+		attentionMask = And(attentionMask, slidingMask)
+	}
 	//for k, node := range cache.Map {
 	//	node.Value.SetLogged(k)
 	//}
